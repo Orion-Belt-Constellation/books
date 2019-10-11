e13f0fccecc945c421b8b69d8c43b9e025456d90
katie <katieb784@gmail.com>
2019-10-11 13:47:44 +1300

    The factory for 50 latin books

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Book;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Book::class, function (Faker $faker) {
+    return [
+
+        'title' => $faker->sentence,
+        'description' => $faker->paragraph,
+        'created_at' => now(),
+        'updated_at' => now(),
+        'author' =>$faker->name(),
+        'isAvailable'=> 1,
+
+    ];
+});
